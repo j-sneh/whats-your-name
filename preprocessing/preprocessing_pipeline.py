fd7659bde57c3ef9eb092f57ee482cbf3c7435b0
Louis Roche <louis.roche@icloud.com>
2025-02-15 16:43:49 +0000

    preprocessing FIEXFDSIJHJBFIOU DSBHFIUDS
    FOIDSIJFBIODSBF

--- preprocessing/preprocessing_pipeline.py
+++ preprocessing/preprocessing_pipeline.py
@@ -1,9 +1,12 @@
 import os
 import cv2
-from preprocessing.audio_processing import extract_audio
+from preprocessing.audio_processing import process_video
 from preprocessing.face_detection import extract_face_embedding
+import database
+import json
+import postprocessing.text_to_speech as tts
 
-def preprocess_video(video_path):
+def preprocess_video(video_path, database):
     """
     Preprocesses the video file to extract audio context and face embeddings.
     
@@ -23,11 +26,14 @@ def preprocess_video(video_path):
         raise FileNotFoundError(f"Video file not found at {video_path}")
     
     # Step 1: Extract audio information (name and context)
-    name, context = extract_audio(video_path)
-    
+    tkt = process_video(video_path=video_path)
+    json_data = json.dumps(tts.extract_info_claude(tkt))
+    name = json_data["name"]
+    context = json_data["other_info"]
     # Step 2: Extract face embedding from the video
     face_embedding = extract_face_embedding(video_path)
-    
+
+
     # Step 3: Return the extracted information
     return {
         "name": name,
