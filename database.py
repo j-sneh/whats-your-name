f31e5788c5b5c0fff4ff35e6c2bb46e69fdcbfcc
Louis Roche <louis.roche@icloud.com>
2025-02-15 15:30:53 +0000

    bob marley main update
    fdsfds

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-
+import numpy as np
+import preprocessing
 class AbstractDatabase(ABC):
     @abstractmethod
     def insert(self, face_vector, name, summary):
@@ -34,3 +35,38 @@ class InMemoryDatabase(AbstractDatabase):
     def delete(self, face_vector):
         if face_vector in self.db:
             del self.db[face_vector]
+
+
+def cosine_similarity(embedding1, embedding2):
+
+    """
+    Computes the cosine similarity between two face embeddings.
+    
+    Args:
+        embedding1 (np.array): First face embedding
+        embedding2 (np.array): Second face embedding. 
+
+    Returns:
+        float: Cosine similarity between the two embeddings.
+    """
+
+    dot_product = np.dot(embedding1, embedding2)
+    norm1 = np.linalg.norm(embedding1)
+    norm2 = np.linalg.norm(embedding2)
+    return dot_product / (norm1 * norm2)
+
+def extract_data_from_face_embedding(database, embedding):
+    keys = np.array(database.keys)
+    similarities = np.zeros(len(keys))
+    best_match = None
+    best_score = 0
+    for index, k in enumerate(keys):
+        similarities[index] = cosine_similarity(k, embedding)
+        if similarities[index] > 0.6 and similarities[index] > best_score:
+            best_score = similarities[index]
+            best_match = k
+    json_data = None
+    if best_match is not None:
+        json_data = database.retrieve(best_match)
+    return json_data
+
